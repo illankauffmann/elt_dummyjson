e1a37be9ebcfd147156aa83f12a764d63ddf11b9
Illan Kauffmann <illankau@gmail.com>
2026-08-06 00:33:05 -0300

    Criando código python para extração de dados de APIs, além de arquivo .gitignore e pasta do ambiente virtual (venv)

---
+++ extract_api.py
@@ -0,0 +1,51 @@
+import requests # Biblioteca para importar dados de api
+
+import pandas as pd
+
+### 1 - criando a função de generica de importação de dados da API
+def importar_dados_api(endpoint, limit=20):
+    """
+    Função para importar dados de uma API e retornar um DataFrame do pandas.
+
+    Parâmetros:
+    endpoint (str): O endpoint da API a ser acessado. Ex: 'products', 'carts', 'users'.
+    limit (int): O número máximo de registros a serem retornados por página. Ex: limit=20 Pega de 20 registros por página.
+    skip (int): O número de registros a serem ignorados (para paginação). Ex: skip=0 Começa do registro 0.
+
+    Retorna:
+    pd.DataFrame: Um DataFrame contendo os dados importados da API.
+    """
+    url_base = f"https://dummyjson.com/{endpoint}"
+    skip = 0
+    data = []
+
+    #Vamos pegar os dados em blocos de 'limit' até que não haja mais dados a serem retornados
+    while True:
+        try:
+            response = requests.get(url_base, params={'limit': limit, 'skip': skip}, timeout=10) #obtendo dados
+        except requests.exceptions.RequestException as e:
+            raise ConnectionError(f"Erro ao conectar na API: {e}")
+        
+        if response.status_code != 200: #not OK
+            raise ConnectionError(f"Falha na requisição. Status code: {response.status_code}")
+
+        try:
+            #response.json() é um dicionário, então pegamos a chave 'endpoint' para obter os dados
+            #response.json()[endpoint] vai retornar uma lista de dicionários, que utilizaremos para criar o DataFrame
+            # Ex de response.json(): {"products": [{dict1}, {dict2},...,{dict100}], "total": 100, "skip": 0, "limit": 30}
+            items = response.json()[endpoint] 
+        except KeyError:
+            raise ValueError(f"A chave '{endpoint}' não foi encontrada na resposta da API.")
+
+        data.extend(items) # concatena os dados obtidos na lista 'data'
+        skip+=limit # atualiza o valor de 'skip' para a próxima página
+
+        if len(items) < limit: #se n_itens < limit, então não haveria mais dados a serem retornados no próximo loop, então podemos sair do loop
+            break
+
+    return pd.DataFrame(data) #transofrma a lista de dicionários em um DataFrame
+
+### 2 - Trazendo os dados via função
+produtos = importar_dados_api('products')
+carts = importar_dados_api('carts')
+users = importar_dados_api('users')
